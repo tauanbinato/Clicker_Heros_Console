9c91deb30927ffb58662d9675c35a60fea771993
tauanbinato <pg10tauan@vfs.local>
2017-04-13 20:08:33 -0700

    adding readme

---
+++ PG10Assignment03_AlejandroGamez/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PG10Assignment03_AlejandroGamez {
+    class Program {
+        static void Main(string[] args) {
+        //Initialize reset bool
+        bool reset = false;
+
+            //Creating a Game variable
+            Game game;
+
+            //While loop to reset the game
+            do {
+                //Clear Screen
+                Console.Clear();
+
+                //Initialize a new game class
+                game = new Game();
+
+                //Read Input, convert to capital letters and save value
+                string input = Input.ReadString("Want to play again?\nYes --- Y\nNo --- N").ToUpper();
+
+                //While loop to ensure correct answer
+                while((!input.Contains('N') && !input.Contains('Y')) || input.Length != 1) {
+                    //Clear screen
+                    Console.Clear();
+
+                    //Read Input, convert to capital letters and save value
+                    input = Input.ReadString("--INVALID INPUT--\nTry again.\n\nWant to play again?\nYes --- Y\nNo --- N").ToUpper();
+                }
+
+                //Assing value depending on user input.
+                reset = input == "Y" ? true : false;
+            } while(reset);
+            Input.Print("\t\t---THANK YOU FOR PLAYING---\n\t\t ---See you next time!!---\n");
+            Input.Pause();
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+"# SpaceBarHeroes" 
+Testing
