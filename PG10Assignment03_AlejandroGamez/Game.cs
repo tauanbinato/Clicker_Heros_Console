6f5621af4b179d860994ff1733ae7a3f9afc9ab7
pg10alejandro <pg10alejandro@PG102>
2017-04-14 18:31:51 -0700

    Added coments and test variables

--- PG10Assignment03_AlejandroGamez/Game.cs
+++ PG10Assignment03_AlejandroGamez/Game.cs
@@ -8,8 +8,15 @@ namespace PG10Assignment03_AlejandroGamez {
     class Game {
         bool isRuning = true;
         private Screen screen = new Screen();
+        //test variables -----------------------------------------------------------------------------------------------------------------------------------
         public static int enemyHealth = 50;
         public static int level = 0;
+        public static string monsName = "Bloody Mary";
+        Random rnd = new Random();
+        public string[] m_MonsterNames = {"Rat","Crazy-B","Fat Dog","Frogy","Big Foot","Nassy","Curly","Yomama","Puririn","ShalOt","Sleepy",
+                                   "Pikachu","Freak Bob","Gordon","Jack Killer","MarcoPolo","Alf","Altas","Samuray Jack","Stinky Pope",
+                                   "Bloody Mary","Beer","Tequila"};
+        //test variables -----------------------------------------------------------------------------------------------------------------------------------
 
         public Game() {
             Console.SetWindowSize(150, 50);
@@ -19,17 +26,16 @@ namespace PG10Assignment03_AlejandroGamez {
                 
                 switch(Input.KeyDown()) {
                     case ConsoleKey.Spacebar:
+                        //All this should be a function inside the monster ---------------------
                         enemyHealth -= 5;
                         if(enemyHealth <= 0) {
+                            monsName = m_MonsterNames[rnd.Next(0, m_MonsterNames.Length)];
                             level++;
                             enemyHealth = 50 * level;
                         }
+                        //----------------------------------------------------------------------
                         screen.ClearScreen();
                         
-                        break;
-
-                    case ConsoleKey.Backspace:
-                        screen.ClearScreen();
                         break;
 
                     case ConsoleKey.Escape:
